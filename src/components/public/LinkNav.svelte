1f9b00c69d39277a0222480c3651e55d493b841d
wawaiguntang <wawaiguntang@gmail.com>
2022-03-21 17:08:56 +0700

    first 2

---
+++ src/components/public/LinkNav.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let text = '';
+	export let link = '';
+	export let active = "false";
+</script>
+
+<a
+	href={link}
+	class="block py-2 pr-4 pl-3 {active == "true"
+		? 'text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white'
+		: 'text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'}"
+	>{text}
+</a>
--- src/components/public/Nav.svelte
+++ src/components/public/Nav.svelte
@@ -1,3 +1,7 @@
+<script>
+    import LinkNav from './LinkNav.svelte';
+    import ButtonNav from './ButtonNav.svelte';
+</script>
 <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
 	<div class="container flex flex-wrap justify-between items-center mx-auto">
 		<a href="/home" class="flex items-center">
@@ -5,22 +9,17 @@
 			<span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">UACAK</span>
 		</a>
 		<div class="flex md:order-2">
-			<button
-				type="button"
-				class="inline-flex text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-				>Login
-				<svg
-					class="ml-2 -mr-1 w-5 h-5"
-					fill="currentColor"
-					viewBox="0 0 20 20"
-					xmlns="http://www.w3.org/2000/svg"
-					><path
-						fill-rule="evenodd"
-						d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-						clip-rule="evenodd"
-					/></svg
-				>
-			</button>
+            <ButtonNav text="Login" icon='<svg
+            class="ml-2 -mr-1 w-5 h-5"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+            ><path
+                fill-rule="evenodd"
+                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                clip-rule="evenodd"
+            /></svg
+        >' action=""/>
 			<button
 				data-collapse-toggle="mobile-menu-4"
 				id="dropdownButton"
@@ -63,32 +62,16 @@
 				aria-labelledby="dropdownButton"
 			>
 				<li>
-					<a
-						href="/jobs"
-						class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
-						aria-current="page">JOBS</a
-					>
+					<LinkNav text="JOBS" link="/jobs" active="true" />
 				</li>
 				<li>
-					<a
-						href="/home"
-						class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-						>COMPANIES</a
-					>
+					<LinkNav text="COMPANIES" link="/companies" active="false" />
 				</li>
 				<li>
-					<a
-						href="/home"
-						class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-						>BLOG</a
-					>
+					<LinkNav text="BLOG" link="/blog" active="false" />
 				</li>
 				<li>
-					<a
-						href="/home"
-						class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-						>CONTACT</a
-					>
+					<LinkNav text="CONTACT" link="/contact" active="false" />
 				</li>
 			</ul>
 		</div>
