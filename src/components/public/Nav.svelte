bdee641122d07ed59110dfa4ce6468cdd1292931
wawaiguntang <wawaiguntang@gmail.com>
2022-03-22 11:25:30 +0700

    bodoamat

--- src/components/public/Nav.svelte
+++ src/components/public/Nav.svelte
@@ -3,11 +3,11 @@
     import ButtonNav from './ButtonNav.svelte';
 	import 'remixicon/fonts/remixicon.css'
 </script>
-<nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
+<nav class="bg-white border-gray-200 xs:px-0 sm:px-4 py-2.5 rounded dark:bg-gray-800">
 	<div class="container flex flex-wrap justify-between items-center mx-auto">
 		<a href="/" class="flex items-center">
 			<img src="/logo.svg" class="mr-3 h-10" alt="UACAK Logo" />
-			<span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">UACAK</span>
+			<span class="xs:invisible sm:visible self-center text-xl font-semibold whitespace-nowrap dark:text-white">UACAK</span>
 		</a>
 		<div class="flex md:order-2">
             <ButtonNav text="Login" icon='<i class="ri-arrow-right-line ml-2"></i>' action=""/>
